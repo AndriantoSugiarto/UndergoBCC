1a94f1df2a67f8a8a32b508a7f45051df6439deb
Andrianto Sugiarto <andriantosugiarto@gmail.com>
2024-09-23 13:48:09 +0700

    implement the latest design from canva

--- src/app/templates/templates.module.ts
+++ src/app/templates/templates.module.ts
@@ -1,18 +1,21 @@
 import { NgModule } from '@angular/core';
 import { CommonModule } from '@angular/common';
 import { NavbarComponent } from './navbar/navbar.component';
+import { FooterComponent } from './footer/footer.component';
 
 
 
 @NgModule({
   declarations: [
-	NavbarComponent
+	NavbarComponent,
+	FooterComponent
   ],
   imports: [
     CommonModule
   ],
   exports: [
-	NavbarComponent
+	NavbarComponent,
+	FooterComponent
   ]
 })
 export class TemplatesModule { }
