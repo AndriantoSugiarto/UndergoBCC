ca8de87f99689e97d3f210cc47038d36a73ea974
Andrianto Sugiarto <andriantosugiarto@gmail.com>
2024-09-25 20:13:24 +0700

    add release prod

---
+++ release-prod.sh
@@ -0,0 +1,16 @@
+#!/bin/bash
+
+BASEPATH=$(dirname $0)
+cd "$BASEPATH"
+
+git checkout build/main
+git pull
+git merge main --no-edit -X theirs
+ionic build --prod -- --base-href https://undergocc.eccbridge.my.id/
+if [ $? -eq 0 ]; then
+	cp htaccess httpdocs/.htaccess
+	git add httpdocs/
+	git commit -m "Build production"
+	git push
+fi
+git checkout main
