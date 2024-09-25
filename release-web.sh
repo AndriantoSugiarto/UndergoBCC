5b0fcf5728dd0427a00f828c43fd04a67d413b92
Andrianto Sugiarto <andriantosugiarto@gmail.com>
2024-09-25 22:04:54 +0700

    update release

--- release-web.sh
+++ release-web.sh
@@ -3,15 +3,6 @@
 BASEPATH=$(dirname $0)
 cd "$BASEPATH"
 
-git checkout build/main
-git pull
-git merge main --no-edit -X theirs
-ionic build --prod -- --base-href https://eccbridge.my.id/
-npx angular-cli-ghpages --dir=httpdocs
-if [ $? -eq 0 ]; then
-	cp htaccess httpdocs/.htaccess
-	git add httpdocs/
-	git commit -m "Build production"
-	git push
-fi
-git checkout main
+ionic build --prod -- --base-href https://undergocc.eccbridge.my.id/
+npx angular-cli-ghpages --dir=www
+
