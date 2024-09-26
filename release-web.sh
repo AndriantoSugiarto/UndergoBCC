84464c9bbc2d3a5f69a99359446ed3b630a55bb9
Andrianto Sugiarto <andriantosugiarto@gmail.com>
2024-09-26 14:50:26 +0700

    update release web to gh pages and styling for container logo text

--- release-web.sh
+++ release-web.sh
@@ -4,5 +4,5 @@ BASEPATH=$(dirname $0)
 cd "$BASEPATH"
 
 ionic build --prod -- --base-href https://undergocc.eccbridge.my.id/
-npx angular-cli-ghpages --dir=www
+npx angular-cli-ghpages --dir=www --cname=undergocc.eccbridge.my.id
 
