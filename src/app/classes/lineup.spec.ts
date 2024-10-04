bf4dec7f3c01baa1982dd978dd480c66c84922f9
Andrianto Sugiarto <andriantosugiarto@gmail.com>
2024-10-04 17:53:01 +0700

    redesign undergo home (not-finished)

---
+++ src/app/classes/lineup.spec.ts
@@ -0,0 +1,7 @@
+import { Lineup } from './lineup';
+
+describe('Lineup', () => {
+  it('should create an instance', () => {
+    expect(new Lineup()).toBeTruthy();
+  });
+});
---
+++ src/app/classes/lineup.ts
@@ -0,0 +1,6 @@
+export class Lineup {
+	bg_image!: string;
+	photo!: string;
+	name!: string;
+	name_2!: string;
+}
